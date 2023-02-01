ede5241845d4105c5fcaae1514629d633c7b3509
Luca Pasquale <lucapasquale06@gmail.com>
2023-02-01 18:36:31 +0100

    added launchctl

--- main.sh
+++ main.sh
@@ -24,7 +24,7 @@ plist="<?xml version='1.0' encoding='UTF-8'?>
     <key>ProgramArguments</key>
     <array>
        <string>/usr/local/bin/dark-mode-notify</string>
-       <string>/Users/$USER/Library/LaunchAgents/change_theme.sh</string>
+       <string>/Users/$USER/Library/LaunchAgents/change-profile.sh</string>
     </array>
 </dict>
 </plist>"
@@ -40,5 +40,14 @@ sed "4s/.*/profile='$lightProfileName'/" change-profile.sh > change-profile.tmp 
 sed "10s/.*/profile='$darkProfileName'/" change-profile.sh > change-profile.tmp && mv change-profile.tmp change-profile.sh
 
 
+
+launchAgentsDir="/Users/$USER/Library/LaunchAgents/"
+
+sudo chmod +x "com.lucapsq.change-profile.plist" "change-profile.sh"
+
+sudo cp com.lucapsq.change-profile.plist change-profile.sh "$launchAgentsDir"
+
+
+launchctl load -w ~/Library/LaunchAgents/com.lucapsq.change-profile.plist
 
 echo "Done."
